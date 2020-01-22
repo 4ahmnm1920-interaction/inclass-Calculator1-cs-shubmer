72795dee065c12d6ce4a433cc8b45cb815cf4bdd
Stefanie Hubmer <stefanie.hubmer@htl-salzburg.ac.at>
2020-01-22 10:48:54 +0100

    my project Return Value

---
+++ Assets/MyGame/Scripts/Calculator.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+
+    public InputField number1;
+    public InputField number2;
+    public Button Add;
+    public Button Subtract;
+    public Button Divide;
+    public Button Multiply;
+    public Text Result;
+    public Text Operator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    public void addNumbers()
+    {
+        string operator1 = "+";
+        float Var1 = float.Parse(number1.text);
+        float Var2 = float.Parse(number2.text);
+        float endResult = Var1 + Var2;
+        Result.text = endResult.ToString();
+        Operator.text = operator1;
+    }
+
+    public void subtractNumbers()
+    {
+        string operator1 = "-";
+        float Var1 = float.Parse(number1.text);
+        float Var2 = float.Parse(number2.text);
+        float endResult = Var1 - Var2;
+        Result.text = endResult.ToString();
+        Operator.text = operator1;
+    }
+
+    public void multiplyNumbers()
+    {
+        string operator1 = "*";
+        float Var1 = float.Parse(number1.text);
+        float Var2 = float.Parse(number2.text);
+        float endResult = Var1 * Var2;
+        Result.text = endResult.ToString();
+        Operator.text = operator1;
+    }
+
+    public void divideNumbers()
+    {
+        string operator1 = "/";
+        float Var1 = float.Parse(number1.text);
+        float Var2 = float.Parse(number2.text);
+        float endResult = Var1 / Var2;
+        Result.text = endResult.ToString();
+        Operator.text = operator1;
+    }
+
+    public void pythagoras()
+    {
+        string operator1 = "py";
+        float Var1 = float.Parse(number1.text);
+        float Var2 = float.Parse(number2.text);
+        float endResult = ((Var1*Var1) + (Var2*Var2));
+        Result.text = endResult.ToString();
+        Operator.text = operator1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MyGame/Scripts/Exercise.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Function1();
+        Debug.Log(Function2(3, 3));
+        Debug.Log(Function3(1.4f, 3.1f));
+        Debug.Log(Function4("Cooler", "Codierer"));
+    }
+
+    public void Function1() 
+    {
+        int number1 = 3;
+        int number2 = 5;
+        Debug.Log(number1 + number2);
+    }
+
+    public int Function2(int verena, int maxl)
+    {
+        return (verena + maxl);
+    }
+
+    public float Function3(float var1, float var2)
+    { 
+        return (var1 + var2);
+    }
+
+    public string Function4(string word1, string word2)
+    {
+        return (word1 + " " + word2);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
